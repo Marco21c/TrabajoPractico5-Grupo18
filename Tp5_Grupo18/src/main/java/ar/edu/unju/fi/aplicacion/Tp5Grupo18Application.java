b228bb8c4a242aedcca5456df9a1a23dad9db132
MarcosC_18 <MarcosC_18@LAPTOP-PF4EE2SM>
2022-05-05 22:59:22 -0300

    Se copio del proyecto tp4 al nuevo proyecto

---
+++ Tp5_Grupo18/src/main/java/ar/edu/unju/fi/aplicacion/Tp5Grupo18Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp5Grupo18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp5Grupo18Application.class, args);
+	}
+
+}
---
+++ Tp5_Grupo18/src/test/java/ar/edu/unju/fi/aplicacion/Tp5Grupo18ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp5Grupo18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
