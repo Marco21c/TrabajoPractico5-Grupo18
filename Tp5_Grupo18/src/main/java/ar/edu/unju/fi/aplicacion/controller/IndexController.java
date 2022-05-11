8f6f3db56e455f2f6470ab077143c27707b90a75
MarcosC_18 <MarcosC_18@LAPTOP-PF4EE2SM>
2022-05-11 18:51:26 -0300

    se agrego independencias de webjars y revisar bootstrap y css

---
+++ Tp5_Grupo18/src/main/java/ar/edu/unju/fi/aplicacion/controller/IndexController.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.aplicacion.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/inicio")
+    public String getIndexPage(Model model){
+    	return "index";
+    }
+    
+}
