9c07089203d6ad937687aa95b1c444ed0155e353
MarcosC_18 <MarcosC_18@LAPTOP-PF4EE2SM>
2022-05-12 04:05:26 -0300

    Nuevos cambios en el proyecto

--- Tp5_Grupo18/src/main/java/ar/edu/unju/fi/aplicacion/controller/IndexController.java
+++ Tp5_Grupo18/src/main/java/ar/edu/unju/fi/aplicacion/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-    @GetMapping("/inicio")
+    @RequestMapping("/inicio")
     public String getIndexPage(Model model){
     	return "index";
     }
