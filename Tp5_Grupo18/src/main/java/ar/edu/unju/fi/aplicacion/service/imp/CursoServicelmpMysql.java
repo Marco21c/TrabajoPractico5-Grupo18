7f6c9b0ebb6f88e67c04deccd3e6f96674e93060
ariel <ariel@DESKTOP-MNIDRE2>
2022-06-05 22:08:30 -0300

    se agrego servicio.imp para myql

---
+++ Tp5_Grupo18/src/main/java/ar/edu/unju/fi/aplicacion/service/imp/CursoServicelmpMysql.java
@@ -0,0 +1,45 @@
+package ar.edu.unju.fi.aplicacion.service.imp;
+
+import ar.edu.unju.fi.aplicacion.model.Curso;
+import ar.edu.unju.fi.aplicacion.service.ICursoService;
+import ar.edu.unju.fi.aplicacion.util.ListaCursos;
+
+public class CursoServicelmpMysql implements ICursoService {
+
+	@Override
+	public Curso getCurso() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ListaCursos getListaCursos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean agregarCurso(Curso curso) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void modificarCurso(Curso curso) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void eliminarCurso(int codigo) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Curso buscarCurso(int codigo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
