3904a15965487dd601e27cb96496308df8bfd81e
MarcosC_18 <MarcosC_18@LAPTOP-PF4EE2SM>
2022-06-10 02:09:46 -0300

    algunos eliminaciones de lineas comentadas

--- Tp5_Grupo18/src/main/java/ar/edu/unju/fi/aplicacion/service/imp/CursoServiceImp.java
+++ Tp5_Grupo18/src/main/java/ar/edu/unju/fi/aplicacion/service/imp/CursoServiceImp.java
@@ -17,54 +17,33 @@ public class CursoServiceImp implements ICursoService {
 ICursosDAO cursosDAOImp; 
     @Override
     public void guardarCurso(Curso curso) {
-	// TODO Auto-generated method stub
     	cursosDAOImp.save(curso);
     }    
 
 	@Override
 	public Curso getCurso() {
-		// TODO Auto-generated method stub
 		return new Curso();
 	}
 
 	@Override
 	public List<Curso> getListaCursos() {
-		// TODO Auto-generated method stub
-		 
 		return cursosDAOImp.findAll();
 	}
 
 	@Override
 	public void modificarCurso(Curso curso) {
-		// TODO Auto-generated method stub
-		
-		/*for(Curso c:listaCursos.getCursos()){
-		 if(c.getCodigo()==curso.getCodigo()) {
-			 c.setCategoria(curso.getCategoria());
-			 c.setCodigo(curso.getCodigo());
-			 c.setFechai(curso.getFechai());
-			 c.setHora(curso.getHora());
-			 c.setModalidad(curso.getModalidad());
-			 c.setTitulo(curso.getTitulo());
-		 }
-		}*/
 		cursosDAOImp.save(curso);
 	}
 
 	@Override
-	public void eliminarCurso(long codigo) {
-		
+	public void eliminarCurso(long codigo) {	
 		cursosDAOImp.deleteById(codigo);
 	}
 
 	@Override
 	public Curso buscarCurso(long codigo) {
-		
 		 Optional<Curso> curso = cursosDAOImp.findById(codigo);
-	
 		return curso.get();
 	}
-
-	
 
 }
